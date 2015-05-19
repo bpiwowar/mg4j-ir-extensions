a68f7dbf24371b06c60ddc62f90a66530fe7858a
Benjamin Piwowarski <benjamin@bpiwowar.net>
2015-05-19 15:47:59 +0200

    Added a sequence term processor

---
+++ src/main/java/net/bpiwowar/mg4j/extensions/tokenizer/SequenceProcessor.java
@@ -0,0 +1,44 @@
+package net.bpiwowar.mg4j.extensions.tokenizer;
+
+import it.unimi.di.big.mg4j.index.TermProcessor;
+import it.unimi.dsi.lang.MutableString;
+import net.bpiwowar.experimaestro.tasks.ClassChooserInstance;
+import net.bpiwowar.experimaestro.tasks.JsonArgument;
+
+import java.util.ArrayList;
+
+/**
+ * Term normalization:
+ * - handle numbers by changing all the digits to 9
+ */
+@ClassChooserInstance(name = "sequence")
+public class SequenceProcessor implements TermProcessor {
+    @JsonArgument
+    ArrayList<TermProcessor> processors;
+
+    @Override
+    public boolean processTerm(MutableString term) {
+        for (int i = 0; i < term.length(); i++) {
+            if (Character.isDigit(term.charAt(i))) {
+                term.setCharAt(i, '9');
+            }
+        }
+
+        if (term.equals('.') || term. equals("``") || term.equals(",")) {
+            return false;
+        }
+
+        term.toLowerCase();
+        return true;
+    }
+
+    @Override
+    public boolean processPrefix(MutableString term) {
+        return processTerm(term);
+    }
+
+    @Override
+    public TermProcessor copy() {
+        return new SequenceProcessor();
+    }
+}
